9d1ec52e11b2ce66be729713774eda55e98f55f3
radibobovich <rxjs500@proton.me>
2023-08-17 14:41:44 +0300

    initial commit

---
+++ lib/src/behavior/scroll_behavior.dart
@@ -0,0 +1,29 @@
+part of easy_refresh;
+
+/// Define [ScrollBehavior] in the scope of EasyRefresh.
+/// Add support for web and PC.
+class ERScrollBehavior extends ScrollBehavior {
+  final ScrollPhysics? _physics;
+
+  const ERScrollBehavior([this._physics]);
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return _physics ?? super.getScrollPhysics(context);
+  }
+
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => <PointerDeviceKind>{
+        PointerDeviceKind.touch,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.invertedStylus,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.unknown,
+      };
+}
